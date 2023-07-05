4c4908fdb73d23f3fbbbcb8f30eb6b14596b70f5
KasamirAnindita <kasamira.anindita.qairia.tik22@mhsw.pnj.ac.id>
2023-07-05 21:03:21 +0700

    edit about us

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -9,8 +9,8 @@ export default function home() {
             <div className='bg-white'>
                 <div className='container mx-auto h-fit flex my-20 marquee relative'>
                     <div className='Poppins-medium flex-auto'>
-                        <h1 className='text-black text-opacity-75 md:w-2/3 text-6xl text-start font-bold bg-slate-600 rounded-t-lg shadow-lg'>WELCOME MESSAGE</h1>
-                        <p className='text-black border-4 border-t-2 p-3 rounded-tr-lg '>
+                        <h1 className='text-black text-opacity-75 md:w-fit text-6xl text-start font-bold bg-slate-600 rounded-t-lg p-3'>WELCOME MESSAGE</h1>
+                        <p className='text-black border-4 border-t-2 p-3 rounded-tr-lg shadow-lg'>
                             <b> Welcome to our Company Profile web page!</b> <br />
                             We are a company committed to innovation and providing the best services. With our professional and experienced team, we are ready to help you achieve your business goals.
                             Explore our company profile to learn more about our history, vision, mission, values, and the services we offer.
@@ -24,17 +24,15 @@ export default function home() {
                     </div>
                 </div>
                 <div className='container h-fit flex my-20 mx-auto marquee1 relative'>
-                <div className='Poppins-medium flex justify-between items-center  '>
+                    <div className='Poppins-medium flex justify-between items-center'>
                         <div className=''>
-                            <img src="http://localhost:3000/Jakun_20230105_115522.jpg" alt="" className='w-full  rounded-lg shadow-lg img_size' />
+                            <img src="http://localhost:3000/Jakun_20230105_115522.jpg" alt="" className='w-full img_size rounded-lg shadow-lg shadow-slate-500' />
                         </div>
-                    <div className=''>
-                        <h1 className='text-black text-opacity-75 text-6xl font-bold bg-slate-600 rounded-t-lg shadow-lg border-r text-end margin-left '>ABOUT US</h1>
-                            <p className='text-black border-4 border-t-2 p-3 rounded-tl-lg'>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <div className='ml-5'>
+                            <h1 className='text-white text-opacity-75 text-6xl font-bold bg-slate-600 rounded-t-lg border-r text-center margin-left p-3'>ABOUT US</h1>
+                            <p className='text-black border-4 border-t-2 p-3 rounded-tl-lg shadow-lg'>
+                            TIWAIBI — another spelling for the TI-B class — is a company that established in 2022 and has 30 experienced and dedicated staff members to make improvement for the company. This company was built in 2022 with the clearest vision and mission.
+                            With a great teamwork, we produce a high-quality products and never dissapoin our costumers.</p>
                         </div>
                     </div>
                 </div>
